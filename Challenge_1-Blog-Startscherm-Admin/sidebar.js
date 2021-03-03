0d979e6fb3a5a4615d14514ea40cf18b73708089
Flexi4b <75685544+Flexi4b@users.noreply.github.com>
2021-03-03 03:38:43 +0100

    Add files via upload

---
+++ Challenge_1-Blog-Startscherm-Admin/sidebar.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function openFunction() {
+    document.getElementById("menu").style.width = "300px";
+}
+
+function closeFunction() {
+    document.getElementById("menu").style.width = "0px";
+}
