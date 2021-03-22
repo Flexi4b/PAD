28c9a44d8b05be71869656f25ed410b5c839eb19
Rik <rik.dek2001@gmail.com>
2021-03-22 22:03:35 +0100

    registratie werkend

--- python/registratie.py
+++ python/registratie.py
@@ -16,44 +16,20 @@ def application(environ, start_response):
         input = environ['wsgi.input'].read().decode()
         params = urlparse.parse_qs(input)
 
-
-# Moet nog juiste gegevens
     db = mysql.connector.connect(
         user='root',
         password='Welkom01',
         host='172.17.0.3',
         database='pad')
-    dbcursor = db.cursor()
 
-    username = params.get('reg-username', [''])[0]
-    password = params.get('reg-password', [''])[0]
-    email = params.get('reg-email', [''])[0]
+    username = params.get('username', [''])[0]
+    password = params.get('password', [''])[0]
+    email = params.get('email', [''])[0]
 
-    query = "SELECT username FROM Speler WHERE username='{fusername}'".format(fusername=username)
-    dbcursor.execute(query)
-    result = dbcursor.fetchall()
+    sql_insert_data = "INSERT INTO `pad`.`Speler` (`username`, `password`, `email`) VALUES ('{}', '{}', '{}')".format(username, password, email)
 
-    if result:
-        html = ''
-        html += '<html>\n'
-        html += ' <head>\n'
-        html += '  <title>registratie</title>\n'
-        html += ' </head>\n'
-        html += ' <body>\n'
-        html += '  <h1>Gebruikersnaan al in gebruik</h1>\n'
-        html += ' </body>\n'
-        html += '</html>\n'
-    else:
-        register = "INSERT INTO Speler (username, password, email) VALUES  ({fuser}, {fpassword}, {femail})".format(fuser=username, fpassword=password, femail=email)
-        dbcursor.execute(register)
-        html = ''
-        html += '<html>\n'
-        html += ' <head>\n'
-        html += '  <title>registratie</title>\n'
-        html += ' </head>\n'
-        html += ' <body>\n'
-        html += '  <h1>welkom</h1>\n'
-        html += ' </body>\n'
-        html += '</html>\n'
-
-    return [bytes(html, 'utf-8')]
+    cursor = db.cursor()
+    cursor.execute(sql_insert_data)
+    db.commit()
+    cursor.close()
+    db.close()
