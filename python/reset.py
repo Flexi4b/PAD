dfec119255fa6a0eb7342fbd7c58b9be44c58022
Takassa <79144209+Takassa@users.noreply.github.com>
2021-04-14 14:54:35 +0200

    Update reset.py

--- python/reset.py
+++ python/reset.py
@@ -1,16 +1,32 @@
+import urllib.parse as urlparse
 import smtplib
 
-sender_email = "syscribe@gmail.com"
-password = "Hacker420"
-rec_email = "iliastakassa@hotmail.com"
-message = """\
-Subject: Reset your password
 
-click on url to change password.\n \nurl here"""
+def application(environ, start_response):
+    status = '200 OK'
+    response_header = [('Content-type', 'text/html')]
+    start_response(status, response_header)
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender_email, password)
-print("Login success")
-server.sendmail(sender_email, rec_email, message)
-print("Email has been sent to ", rec_email)
+    method = environ.get('REQUEST_METHOD', '')
+
+    params = {}
+    if method == 'GET':
+        params = urlparse.parse_qs(environ['QUERY_STRING'])
+    elif method == 'POST':
+        input = environ['wsgi.input'].read().decode()
+        params = urlparse.parse_qs(input)
+
+    rec_email = params.get('username', [''])[0]
+
+    sender_email = "syscribe@gmail.com"
+    password = "Hacker420"
+    message = """\
+    Subject: Reset your password
+    click on url to change password.\n \nurl here"""
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, password)
+    print("Login success")
+    server.sendmail(sender_email, rec_email, message)
+    print("Email has been sent to ", rec_email)
